5698f5ced403a2fcb6970784c533027825f69973
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2026-02-17 23:54:33 +0000

    added adsense meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Google AdSense */}
+        <meta name="google-adsense-account" content="ca-pub-1589967275136790" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1589967275136790"
